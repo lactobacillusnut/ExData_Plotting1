a2e3e63f8ac2e052ee1a3ce94a889dfa47dd6bb9
lactobacillusnut <lactobacillus88@gmail.com>
2020-06-02 21:32:38 +0800

    Script for Plot 4 complete. adjusted the legend so that it is not cut off, by using inset values.

---
+++ plot4.R
@@ -0,0 +1,32 @@
+## This script creates Plot 1 in the Exploratory Data Analysis Course
+
+# download file if not already done so
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if(!file.exists("exdata_data_household_power_consumption.zip")) 
+    download.file(fileurl,
+                  "exdata_data_household_power_consumption.zip")
+
+# Check if file has been unzipped
+if(!file.exists("household_power_consumption.txt")){
+    unzip("exdata_data_household_power_consumption.zip")
+}
+
+# load libraries
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(lubridate)
+
+# read data as tibble
+data <- tbl_df(read.table("household_power_consumption.txt",sep = ";",header = 1)) 
+
+# filter by dates "2007-02-01" to "2007-02-02"
+newdata <- data %>%
+    mutate(DateTime=dmy_hms(paste(Date,Time))) %>% #combine data and time
+    mutate(Date=dmy(Date)) %>%
+    mutate(Global_active_power=as.numeric(Global_active_power),
+           Sub_metering_1=as.numeric(Sub_metering_1),
+           Sub_metering_2=as.numeric(Sub_metering_2),
+           Sub_metering_3=as.numeric(Sub_metering_3)) %>%
+    filter(Date >= ymd("2007-02-01") & Date <= ymd("2007-02-02") & !is.na(Sub_metering_1))
+
